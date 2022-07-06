5b70dfeb57b9e26c23595188185f8bbab05bda26
ShvetsRoman <shvets33@gmail.com>
2022-07-06 19:49:33 +0300

    add

--- prog/conf/nvim/lua/custom/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- ["nvim-lualine/lualine.nvim"] = {
-  --   opt = true,
-  --   config = function()
-  --     require = "kyazdani42/nvim-web-devicons"
-  --     require = "custom.plugins.lualine"
-  --   end,
-  -- },
-}
