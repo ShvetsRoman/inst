ae546decbd29191552550db1bcbecfbdcb3b5300
ShvetsRoman <shvets33@gmail.com>
2025-12-12 00:02:32 +0200

    add

--- prog/inst_po_kde.sh
+++ prog/inst_po_kde.sh
@@ -35,4 +35,5 @@ sudo pacman -Syu --noconfirm --needed libreoffice-still libreoffice-still-uk
 
 ### Install AUR ###
 # pikaur -S deadbeef google-chrome
-pikaur -S  sayonara-player google-chrome
+pikaur -S  google-chrome
+pikaur -S  sayonara-player
